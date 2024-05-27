6e4b846d424b8595d202c6d81d3f608f261119fd
iliay28 <iliaydos28@gmail.com>
2024-05-27 17:35:00 +0000

    чётко

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import os
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/mondhtat")
+def world():
+    return render_template("Мондштат.html", title="Hello")
+
+@app.route("/otziv")
+def creat():
+    files_paths = os.listdir('./files')
+    list_files = []
+    for file_name in files_paths:
+        file_content = open(f'./files/{file_name}', 'r').readlines()
+        list_files.append([file_name, file_content])
+    return render_template("otziv.html", list_files=list_files)
+
+@app.route('/show', methods=['GET', 'POST'])
+def show():
+    if request.method == 'POST':
+        file_name = request.form.get('file_name')
+        file_context = request.form.get('file_context')
+        fw = open(f'./files/{file_name}', 'w')
+        fw.write(file_context)
+        fw.close()
+    return render_template('show.html', message=file_name)
